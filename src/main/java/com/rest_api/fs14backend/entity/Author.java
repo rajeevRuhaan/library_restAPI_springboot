9194d4b75c20f00fcf2661c1a7edf2d7bfb43a0a
rajeevRuhaan <urrajivin@hotmail.com>
2023-05-02 23:09:46 +0300

    author entity and repository created

--- src/main/java/com/rest_api/fs14backend/entity/Author.java
+++ src/main/java/com/rest_api/fs14backend/entity/Author.java
@@ -1,2 +1,33 @@
-package com.rest_api.fs14backend.entity;public class Author {
+package com.rest_api.fs14backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="author")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Author {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String firstName;
+    @Column(nullable = false, columnDefinition = "varchar(50)")
+    private String lastName;
+    @Column(nullable = true, columnDefinition = "varchar(50)")
+    private String email;
+    @Column(nullable = true, columnDefinition = "varchar(50)")
+    private String phone;
+
+
 }
