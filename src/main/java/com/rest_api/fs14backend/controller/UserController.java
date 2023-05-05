620197de3aa1b618dcb4231321686dbb2437c6c9
Mughees0 <abdul.mughees009@gmail.com>
2023-05-05 15:49:42 +0300

    Changed the mapping for userController

--- src/main/java/com/rest_api/fs14backend/controller/UserController.java
+++ src/main/java/com/rest_api/fs14backend/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("api/v1/users")
 public class UserController {
     @Autowired
     private UserServiceImpl userService;
@@ -22,14 +22,14 @@ public class UserController {
         return userService.findAll();
     }
 
-    @PostMapping("/signup")
-    public List<User> signup(){
-        return userService.signup();
-    }
-
-    @PostMapping("/signin")
-    public String signin(){
-        return userService.signin();
-    }
+//    @PostMapping("/signup")
+//    public List<User> signup(){
+//        return userService.signup();
+//    }
+//
+//    @PostMapping("/signin")
+//    public String signin(){
+//        return userService.signin();
+//    }
 
 }
