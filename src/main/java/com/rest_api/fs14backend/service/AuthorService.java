ec2a59864f1452fbf1420eaae62828b4929c6d83
rajeevRuhaan <urrajivin@hotmail.com>
2023-05-05 11:57:46 +0300

    author controller and service implemented for CRUD

---
+++ src/main/java/com/rest_api/fs14backend/service/AuthorService.java
@@ -0,0 +1,14 @@
+package com.rest_api.fs14backend.service;
+
+import com.rest_api.fs14backend.entity.Author;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AuthorService {
+    UUID createAuthor(Author author);
+    Author getUserById(UUID authorId);
+    List<Author> getAllAuthors();
+    Author updateAuthor(Author author);
+    void deleteAuthor(UUID authorId);
+}
---
+++ src/main/java/com/rest_api/fs14backend/serviceImpl/AuthorServiceImpl.java
@@ -0,0 +1,49 @@
+package com.rest_api.fs14backend.serviceImpl;
+
+import com.rest_api.fs14backend.entity.Author;
+import com.rest_api.fs14backend.repository.AuthorRepository;
+import com.rest_api.fs14backend.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+    @Autowired
+    private AuthorRepository authorRepository;
+    @Override
+    public UUID createAuthor(Author author) {
+        UUID id = authorRepository.save(author).getId();
+        return id;
+    }
+
+    @Override
+    public Author getUserById(UUID authorId) {
+        Optional<Author> optionalAuthor = authorRepository.findById(authorId);
+        return optionalAuthor.get();
+    }
+
+    @Override
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        Author existingAuthor = authorRepository.findById(author.getId()).get();
+        existingAuthor.setEmail(author.getEmail());
+        existingAuthor.setFirstName(author.getFirstName());
+        existingAuthor.setLastName(author.getLastName());
+        existingAuthor.setPhone(author.getPhone());
+        return existingAuthor;
+    }
+
+    @Override
+    public void deleteAuthor(UUID authorId) {
+    authorRepository.deleteById(authorId);
+
+    }
+}
